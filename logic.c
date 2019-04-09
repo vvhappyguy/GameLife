a8421532cf346ec35e995ca1e3581246aadde614
IoKin <vvhappyguy@gmail.com>
2019-04-09 21:57:11 +0300

    Added basic game logic files.

---
+++ logic.c
@@ -0,0 +1,5 @@
+//
+// Created by dyaki on 3/22/2019.
+//
+
+#include "logic.h"
---
+++ logic.h
@@ -0,0 +1,8 @@
+//
+// Created by dyaki on 3/22/2019.
+//
+
+#ifndef GAMELIFE_LOGIC_H
+#define GAMELIFE_LOGIC_H
+
+#endif //GAMELIFE_LOGIC_H
